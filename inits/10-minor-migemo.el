bdda1be22b77da1e65c75401528cb6da82718bd1
Taku Watabe <taku.eof@gmail.com>
2019-01-12 15:51:38 +0900

    Fixed 'migemo' auto keybindings break default 'yank' and other keybindings.

--- inits/10-minor-migemo.el
+++ inits/10-minor-migemo.el
@@ -1,7 +1,7 @@
 ;;; 10-minor-migemo.el --- 設定 - マイナーモード - ローマ字入力から日本語をインクリメンタル検索
 
 ;; Copyright (C) 2013-2019 Taku Watabe
-;; Time-stamp: <2019-01-09T11:35:01+09:00>
+;; Time-stamp: <2019-01-12T15:48:51+09:00>
 
 ;;; Commentary:
 
@@ -18,10 +18,13 @@
  '(migemo-white-space-regexp "[[:space:]\s-]*")
  ;; ユーザ別基礎ディレクトリは設定ディレクトリ内にまとめる
  '(migemo-directory (locate-user-emacs-file "migemo"))
+ ;; `migemo' 側で定義されている `isearch' 関連キーバインドを使わせない
+ ;; ミニバッファ内で `yank' できない現象が発生する問題の対策
+ '(migemo-use-default-isearch-keybinding nil)
  ;; キャッシュを使わせる
  '(migemo-use-pattern-alist t)
- '(migemo-pattern-alist-length 1024)
  '(migemo-use-frequent-pattern-alist t)
+ '(migemo-pattern-alist-length 1024)
  ;; キャッシュはユーザ別基礎ディレクトリにまとめる
  '(migemo-pattern-alist-file (expand-file-name ".migemo-patterns" migemo-directory))
  '(migemo-frequent-pattern-alist-file (expand-file-name ".migemo-frequent-patterns" migemo-directory))
@@ -32,9 +35,13 @@
 ;; ----------------------------------------------------------------------------
 ;; 起動
 ;; ----------------------------------------------------------------------------
-(if (and (require 'migemo nil :noerror)
-         (fboundp 'migemo-init))
-    (migemo-init))
+(when (require 'migemo nil :noerror)
+  (if (and (boundp 'isearch-mode-map)
+           (fboundp 'migemo-isearch-toggle-migemo))
+      (define-key isearch-mode-map (kbd "C-c C-s") #'migemo-isearch-toggle-migemo))
+
+  (if (fboundp 'migemo-init)
+      (migemo-init)))
 
 
 ;; ----------------------------------------------------------------------------
