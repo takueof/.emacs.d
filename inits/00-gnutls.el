e10a7245dfca1cc0d8dd2b21c65196db3b774d6a
Taku Watabe <taku.eof@gmail.com>
2019-01-13 12:22:53 +0900

    Very little comment change.

--- inits/00-gnutls.el
+++ inits/00-gnutls.el
@@ -1,11 +1,11 @@
 ;;; 00-gnutls.el --- 設定 - GnuTLS -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2014-2019 Taku Watabe
-;; Time-stamp: <2019-01-13T00:25:22+09:00>
+;; Time-stamp: <2019-01-13T12:18:52+09:00>
 
 ;;; Commentary:
 
-;; GNU Linux/UNIX 以外の環境でパスが認識されず、フリーズする問題を回避する。
+;; GNU/Linux ならびに UNIX 以外の環境でパスが認識されず、フリーズする問題を回避する。
 ;;
 ;; see also:
 ;; https://twitter.com/robario/statuses/364694728299659264
