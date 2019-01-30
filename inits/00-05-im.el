cbd020cc7338eb5d271bda8af42199f9d0e6d798
Taku Watabe <taku.eof@gmail.com>
2019-01-30 22:45:53 +0900

    Fixed 'advice-add' error.

--- inits/00-05-im.el
+++ inits/00-05-im.el
@@ -1,7 +1,7 @@
 ;;; 00-05-im.el --- 設定 - インプットメソッド -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2019 Taku Watabe
-;; Time-stamp: <2019-01-30T14:41:29+09:00>
+;; Time-stamp: <2019-01-30T22:40:05+09:00>
 
 ;;; Commentary:
 
@@ -20,22 +20,26 @@
      ;; インプットメソッド切替時に、フェイス `cursor' を変更
      ;; -----------------------------------------------------------------------
      (defface my-cursor-default nil
-       "`current-input-method' が non-nil の場合に用いる `cursor' フェイス。"
+       "`cursor' face for `current-input-method' is nil."
        :group 'customize)
      (copy-face 'cursor 'my-cursor-default)
 
      (defface my-cursor-input-method-activated '((t
                                                   :background "red"))
-       "`current-input-method' が non-nil の場合に用いる `cursor' フェイス。"
+       "`cursor' face for `current-input-method' is non-nil."
        :group 'customize)
 
-     (defun my-change-cursor-faces-by-current-input-method (window &optional norecord)
-       "`current-input-method' の評価値に応じ、カーソル色を変更する。"
+     (defun my-change-cursor-faces-by-current-input-method ()
+       "Change cursor color with `current-input-method'."
        (set-cursor-color
         (face-attribute (if current-input-method
                             'my-cursor-input-method-activated
                           'my-cursor-default)
                         :background)))
+
+     (defun my-change-cursor-faces-by-current-input-method-advice (window &optional norecord)
+       "Change cursor color with `current-input-method' for `advice-add'."
+       (my-change-cursor-faces-by-current-input-method))
 
 
      ;; -----------------------------------------------------------------------
@@ -47,19 +51,22 @@
                #'my-change-cursor-faces-by-current-input-method)
 
      ;; ウインドウ選択後、input-method の状態に応じてフェイス `cursor' を変更
+     ;;
      ;; `cursor' はフレーム単位
      ;; しかし、`current-input-method' はバッファローカル変数
-     ;; ゆえに、バッファ間で `current-input-method' 値が異なれば、
+     ;; よって、バッファ間で `current-input-method' 値が異なれば、
      ;; `cursor' が意図せぬ状態になる
+     ;;
      ;; ゆえに、ウインドウ切替のタイミングでの `cursor' 明示変更が必要
+     ;;
      ;; バッファ切替時は、特に何もしない
-     ;; ウインドウ切替時と異なり、ユーザに対しては明示的カーソル表示は未発生
+     ;;
      ;; `select-window' の実行後に起動するフックが存在しないため、
      ;; アドバイスでしのぐ
      (if (fboundp 'select-window)
          (advice-add 'select-window
                      :after
-                     #'my-change-cursor-faces-by-current-input-method))))
+                     #'my-change-cursor-faces-by-current-input-method-advice))))
 
 
 ;; ----------------------------------------------------------------------------
