095d2ea6d7d652c514ed809edde6f8162dd8194c
Taku Watabe <taku.eof@gmail.com>
2019-09-28 15:24:38 +0900

    Implement 'typescript-mode' settings.

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2019 Taku Watabe
-;; Time-stamp: <2019-09-21T14:46:11+09:00>
+;; Time-stamp: <2019-09-28T15:21:55+09:00>
 
 ;; Author: Taku Watabe <taku.eof@gmail.com>
 
@@ -2526,6 +2526,26 @@ Ordering is lexicographic."
          :init
          (defun my-php-mode-initialize ()
            "Initialize `php-mode' before file load."
+           (setq-local indent-tabs-mode nil)
+
+           ;; EditorConfig 対応
+           (eval-after-load 'editorconfig
+             '(if (hash-table-p editorconfig-properties-hash)
+                  (let* ((indent-style-data (gethash 'indent_style editorconfig-properties-hash))
+                         (indent-style (equal indent-style-data "tab")))
+                    (if (not (equal indent-tabs-mode indent-style))
+                        (setq-local indent-tabs-mode indent-style)))))))
+
+
+       ;; ---------------------------------------------------------------------
+       ;; TypeScript
+       ;; ---------------------------------------------------------------------
+       (leaf typescript-mode
+         :package t
+         :hook ((typescript-mode-hook . my-typescript-mode-initialize))
+         :init
+         (defun my-typescript-mode-initialize ()
+           "Initialize `typescript-mode' before file load."
            (setq-local indent-tabs-mode nil)
 
            ;; EditorConfig 対応
