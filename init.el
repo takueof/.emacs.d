c7beac5e701f6ecfe88fa9de6bb2677eab955f58
takueof <taku.eof@gmail.com>
2022-04-28 09:26:32 +0900

    Add lsp-mode for GraphQL support

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2022 Taku Watabe
-;; Time-stamp: <2022-04-27T17:31:09+09:00>
+;; Time-stamp: <2022-04-28T09:21:46+09:00>
 
 ;; Author: Taku Watabe <taku.eof@gmail.com>
 
@@ -2063,6 +2063,7 @@ Ordering is lexicographic."
     (leaf lsp-mode
       :package t
       :hook (;; 有効化は必要最小限にとどめる
+             (graphql-mode-hook . lsp)
              (js-mode-hook . lsp)
              (js2-mode-hook . lsp)
              (php-mode-hook . lsp)
