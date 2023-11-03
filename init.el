a6c822660d76573c7a970acfa8f3515b50674a78
Taku Watabe <taku.eof@gmail.com>
2023-11-03 16:51:09 +0900

    Set magit custom options

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2023 Taku Watabe
-;; Time-stamp: <2023-11-03T16:33:06+09:00>
+;; Time-stamp: <2023-11-03T16:50:56+09:00>
 
 ;; Author: Taku Watabe <taku.eof@gmail.com>
 
@@ -813,7 +813,8 @@
   ;; --------------------------------------------------------------------------
   (leaf magit
     :package t
-    :bind (("C-x g" . magit-status)))
+    :bind (("C-x g" . magit-status))
+    :custom ((auto-revert-buffer-list-filter . #'magit-auto-revert-buffer-p)))
 
 
   ;; --------------------------------------------------------------------------
