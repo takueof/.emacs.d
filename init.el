8f1ec466a1898efbef83d2a7b060aa1cbd49be12
Taku Watabe <taku.eof@gmail.com>
2023-12-03 13:05:15 +0900

    Enable built-in package upgrade

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2023 Taku Watabe
-;; Time-stamp: <2023-12-03T09:51:40+09:00>
+;; Time-stamp: <2023-12-03T13:05:08+09:00>
 
 ;; Author: Taku Watabe <taku.eof@gmail.com>
 
@@ -414,12 +414,12 @@
 (when (require 'package nil :noerror)
   ;; 確実に定義された後で追加
   (add-to-list 'package-archives '("MELPA" . "https://melpa.org/packages/"))
-
   ;; あらゆるパッケージロードに先んじての記述が必須
   (package-initialize)
-
   ;; `list-packages' のような短縮版を用意
-  (defalias 'list-packages-no-fetch 'package-list-packages-no-fetch))
+  (defalias 'list-packages-no-fetch 'package-list-packages-no-fetch)
+  ;; ビルトインパッケージの更新を許可
+  (custom-set-variables '(package-install-upgrade-built-in t)))
 
 
 ;; ============================================================================
