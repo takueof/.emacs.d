77b2d0159aeaa802b368800b176bcef91c97c5c1
takueof <taku.eof@gmail.com>
2023-12-12 18:40:13 +0900

    Swap 'Symbola' font priorities

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2023 Taku Watabe
-;; Time-stamp: <2023-12-12T14:24:39+09:00>
+;; Time-stamp: <2023-12-12T18:39:47+09:00>
 
 ;; Author: Taku Watabe <taku.eof@gmail.com>
 
@@ -2980,9 +2980,9 @@
                               'emoji
                               (font-spec :size font-size
                                          :family (my-fallback-font-family "Apple Color Emoji"
+                                                                          "Symbola"
                                                                           "Segoe UI Emoji"
                                                                           "Segoe UI Symbol"
-                                                                          "Symbola"
                                                                           "Monospace"))))
 
 
