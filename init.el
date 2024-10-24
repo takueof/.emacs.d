16d07c0e7db7465e59c14f15555d7c14b9e3da71
Taku Watabe <taku.eof@gmail.com>
2024-10-25 00:20:10 +0900

    Change from "load-theme" to "modus-themes-load-theme"

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2024 Taku Watabe
-;; Time-stamp: <2024-10-21T13:32:17+09:00>
+;; Time-stamp: <2024-10-25T00:19:24+09:00>
 
 ;; Author: Taku Watabe <taku.eof@gmail.com>
 
@@ -475,7 +475,7 @@
                                                       (border-mode-line-inactive unspecified)
                                                       (bg-mode-line-active bg-green-subtle))))
   :config
-  (load-theme 'modus-vivendi :no-confirm))
+  (modus-themes-load-theme 'modus-vivendi))
 
 
 ;; ============================================================================
@@ -1868,7 +1868,6 @@
                                             (space-mark ?\u3000 [?\u25a1] [?_ ?_])
                                             ;; Tab -> RIGHT-POINTING DOUBLE ANGLE QUOTATION MARK
                                             (tab-mark ?\t [?» ?\t] [?\\ ?\t]))))
-  :custom-face ((whitespace-space . '((t (:background nil)))))
   :init
   (defun my-whitespace-mode-initialize ()
     "Initialize `whitespace'."
