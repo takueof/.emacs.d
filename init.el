385a4f923fcff577967d2fdde43108891a09c5c3
Taku Watabe <taku.eof@gmail.com>
2026-02-16 13:39:59 +0900

    Workaround a bug in "jupyter-repl.el".
    (remq 'field yank-excluded-properties) in "jupyter-repl.el" to avoid the problem of elisp error when setting t, which is allowed to be given to yank-excluded-properties.

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2026 Taku WATABE
-;; Time-stamp: <2026-02-16T13:25:46+09:00>
+;; Time-stamp: <2026-02-16T13:36:13+09:00>
 
 ;; Author: Taku Watabe <taku.eof@gmail.com>
 
@@ -176,10 +176,6 @@
  ;; クリップボードと `kill-ring' を同期させる
  ;;
  '(select-enable-clipboard t)
- ;;
- ;; Yank (Paste) 時にプロパティを全破棄し、プレーンテキストを利用
- ;;
- '(yank-excluded-properties t)
  ;;
  ;; プレビューウインドウの表示を即時にする
  ;;
