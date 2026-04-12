d9bb789e3355cbe24c0d18e334a893ce30d6e0d9
Taku Watabe <taku.eof@gmail.com>
2026-04-12 20:33:11 +0900

    Remove unused "keyboard-translate"

--- init.el
+++ init.el
@@ -984,10 +984,8 @@
          ;; フレーム背景の透明度を切り替える
          ("C-c w t" . my-toggle-frame-transparency))
   :config
-  ;; <Backspace> と <DEL> を 交換する
+  ;; C-h を backspace とみなす
   (keyboard-translate ?\C-h ?\C-?)
-  ;; <DEL> を <C-d> にする
-  (keyboard-translate ?\C-? ?\C-d)
   ;; 誤字の元になる `transpose-chars' 実行を抑制する
   (global-unset-key (kbd "C-t"))
   ;; `ido-undo-merge-work-directory' 実行のため <C-z> を押しすぎた場合、
