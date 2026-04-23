d370579b6803f3ec379fa568d501199502ce3145
Taku Watabe <taku.eof@gmail.com>
2026-04-23 11:15:28 +0900

    Add vterm-mode to whitespace-mode ignored list

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2026 Taku WATABE
-;; Time-stamp: <2026-04-23T11:08:08+09:00>
+;; Time-stamp: <2026-04-23T11:12:39+09:00>
 
 ;; Author: Taku WATABE <taku.eof@gmail.com>
 
@@ -906,7 +906,8 @@
     (with-eval-after-load 'whitespace
       (if (member major-mode '(;; 降順ソート
                                agent-shell-mode
-                               lisp-interaction-mode))
+                               lisp-interaction-mode
+                               vterm-mode))
           (whitespace-mode -1))))
   :global-minor-mode global-whitespace-mode)
 
