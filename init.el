677e044ff946c2b872ba7195c16cad88b1915eca
Taku Watabe <taku.eof@gmail.com>
2026-07-11 02:59:07 +0900

    Remove eldoc-idole-delay config (use default value)

--- init.el
+++ init.el
@@ -1,7 +1,7 @@
 ;;; init.el --- "GNU Emacs" main config file -*- mode: Emacs-Lisp; coding: utf-8-unix; lexical-binding: t; -*-
 
 ;; Copyright (C) 2013-2026 Taku WATABE
-;; Time-stamp: <2026-07-11T02:50:18+09:00>
+;; Time-stamp: <2026-07-11T02:58:59+09:00>
 
 ;; Author: Taku WATABE <taku.eof@gmail.com>
 
@@ -315,7 +315,6 @@
 ;; `eldoc'
 ;;
 (setopt eldoc-minor-mode-string nil)
-(setopt eldoc-idle-delay 0.25)
 (setopt eldoc-echo-area-use-multiline-p #'truncate-sym-name-if-fit)
 ;;
 ;; `elec-pair'
